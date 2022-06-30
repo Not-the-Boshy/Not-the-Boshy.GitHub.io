daa1a5fc9e7fa5eaacf4e901f59e3b9e4f512518
Not-the-Boshy <drummerboyrocket@gmail.com>
2022-06-30 02:13:10 +0000

    Stuck on paddles factory function

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -10,7 +10,8 @@ function runProgram(){
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  var speedX = 0;
+  var speedY = 0;
   // Game Item Objects
   
   var KEY = {
@@ -18,21 +19,23 @@ function runProgram(){
     UP: 38, DOWN: 40. // P2
   }
   // Paddle obj (no speed X required because it just moves up and down and not side-to-side)
-var paddle ={};
-  paddle.x = Number($(".paddle").css("right").replace(/[^-\d\.]/g, ''))
-  paddle.y = Number($(".paddle").css("top").replace(/[^-\d\.]/g, ''))
-  paddle.speedY = speedY
 
-  // hardcoded, doesn't need to be changed
-  paddle.height = $(".paddle").height
-  paddle.width = $(".paddle").width
+  function Paddle(paddleNumber, speedY){
+    paddleNumber = {};
+    paddleNumber.id = $(paddleNumber).id,
+    console.log(paddleNumber.id),
+    paddleNumber.y = Number($(".paddle").css("top").replace(/[^-\d\.]/g, '')),
+    paddleNumber.speedY = speedY,
+    paddleNumber.height = $(".paddle").height,
+    paddleNumber.width = $(".paddle").width;
 
-  console.log(paddle);
+    return paddleNumber;
+  }
 
   // Ball obj
 var ball = {};
-  ball.x = Number($("#ball").css("right").replace(/[^-\d\.]/g, ''))
-  ball.y = Number($("#ball").css("top").replace(/[^-\d\.]/g, ''))
+  ball.x = Number($("#ball").css("right").replace(/[^-\d\.]/g, '')) + ball.speedX
+  ball.y = Number($("#ball").css("top").replace(/[^-\d\.]/g, '')) + ball.speedY
   ball.speedX = speedX
   ball.speedY = speedY
   ball.height = $("#ball").height
@@ -40,7 +43,7 @@ var ball = {};
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on("keydown", handleKeydown);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -50,9 +53,6 @@ var ball = {};
   On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
   by calling this function and executing the code inside.
   */
-
-  $(document).on("keydown",handleKeyDown);
-
   function newFrame() {
     moveBall();
   }
@@ -60,27 +60,34 @@ var ball = {};
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
 
+
+  function handleKeydown(event){
+    if (event.which === KEY.W){
+      Paddle(paddleLeft, 5);
+    };
+    if (event.which === KEY.S){
+      Paddle(paddleLeft, -5);
+    };
+    if (event.which === KEY.UP){
+      Paddle(paddleRight, 5);
+    };
+    if (event.which === KEY.DOWN){
+      Paddle(paddleRight, -5);
+    };
   }
 
-  function moveBall(){
-    ball.x += ball.speedX;              // update ball X pos var
-    $(ball.id).css("left", ball.x);     // update ball Y pos onscreen
-    ball.y += ball.speedY;              // update ball Y pos var
-    $(ball.id).css("top", ball.x);      // update ball Y pos onscreen
-  }
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  function handleKeydown(event){
-    if (event.which === KEY.W || event.which === KEY.UP){
-      paddle.speedY =+ -5;
-    };
-    if (event.which === KEY.A || event.which === KEY.DOWN){
-      paddle.speedY =+ -5;
-    };
+  function moveBall(){
+    // Repositions Ball
+    ball.x += ball.speedX;              // update ball X pos var
+    ball.y += ball.speedY;              // update ball Y pos var
+    // Redraws Ball
+    $(ball.id).css("left", ball.x);     // update ball Y pos onscreen
+    $(ball.id).css("top", ball.x);      // update ball Y pos onscreen
   }
 
   function endGame() {
