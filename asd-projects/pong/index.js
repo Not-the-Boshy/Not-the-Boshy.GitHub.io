3be607c46b9e0d849f7d2575e119f200c61addf4
Not-the-Boshy <drummerboyrocket@gmail.com>
2022-06-30 01:01:15 +0000

    More was done but nothing's moving for now. I sure ought to push when I actually have made progress but I do want to save progress now and again, just in case.

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -12,7 +12,31 @@ function runProgram(){
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
   // Game Item Objects
+  
+  var KEY = {
+    W: 87, S: 83,     // P1
+    UP: 38, DOWN: 40. // P2
+  }
+  // Paddle obj (no speed X required because it just moves up and down and not side-to-side)
+var paddle ={};
+  paddle.x = Number($(".paddle").css("right").replace(/[^-\d\.]/g, ''))
+  paddle.y = Number($(".paddle").css("top").replace(/[^-\d\.]/g, ''))
+  paddle.speedY = speedY
 
+  // hardcoded, doesn't need to be changed
+  paddle.height = $(".paddle").height
+  paddle.width = $(".paddle").width
+
+  console.log(paddle);
+
+  // Ball obj
+var ball = {};
+  ball.x = Number($("#ball").css("right").replace(/[^-\d\.]/g, ''))
+  ball.y = Number($("#ball").css("top").replace(/[^-\d\.]/g, ''))
+  ball.speedX = speedX
+  ball.speedY = speedY
+  ball.height = $("#ball").height
+  ball.width = $("#ball").height
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
@@ -26,9 +50,11 @@ function runProgram(){
   On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
   by calling this function and executing the code inside.
   */
-  function newFrame() {
-    
 
+  $(document).on("keydown",handleKeyDown);
+
+  function newFrame() {
+    moveBall();
   }
   
   /* 
@@ -38,11 +64,25 @@ function runProgram(){
 
   }
 
+  function moveBall(){
+    ball.x += ball.speedX;              // update ball X pos var
+    $(ball.id).css("left", ball.x);     // update ball Y pos onscreen
+    ball.y += ball.speedY;              // update ball Y pos var
+    $(ball.id).css("top", ball.x);      // update ball Y pos onscreen
+  }
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  
+  function handleKeydown(event){
+    if (event.which === KEY.W || event.which === KEY.UP){
+      paddle.speedY =+ -5;
+    };
+    if (event.which === KEY.A || event.which === KEY.DOWN){
+      paddle.speedY =+ -5;
+    };
+  }
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
