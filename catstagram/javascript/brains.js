f87ff60f50e56cee91af252ef46929e8e3f4a1cd
Not-the-Boshy <drummerboyrocket@gmail.com>
2022-07-06 20:47:22 +0000

    Basic Spinstagram complete, now I need to make it look nice

--- catstagram/javascript/brains.js
+++ catstagram/javascript/brains.js
@@ -1,6 +1,21 @@
+let user = {
+    firstName: "Shivom",
+    lastName: "Thanki",
+    followerCount: 4,
+    pfp: "https://cdn.dribbble.com/users/518512/screenshots/4389094/media/8da94f976d1886ed66d71ea57a234043.png",
+}
+const timer = 1;
+
+setTimeout(function(){ document.getElementById("Name").innerHTML = "Name: " + user.firstName + " " + user.lastName;}, timer);
+setTimeout(function(){ document.getElementById("followerCount").innerHTML = "Followers: " + user.followerCount;}, timer);
+setTimeout(function(){ $("#pfp").attr("src", user.pfp);}, timer);
+
 let likeCounter = 0;
 
-function likePhoto(counterNumber){
+function likePhoto(id){
     likeCounter++
     console.log("Photo liked " + likeCounter + " times!");
+    document.getElementById(id).innerHTML = likeCounter;
+    $("button").text("Likes: " + likeCounter);
 }
+
